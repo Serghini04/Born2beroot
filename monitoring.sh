8f1fab2fcb450ca0300b7030927769a4d2ffa263
Serghini04 <91221718+Serghini04@users.noreply.github.com>
2023-12-24 13:40:22 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
-ARCH=$(uname -srvmo)
-PCPU=$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
-VCPU=$(grep processor /proc/cpuinfo | uniq | wc -l)
-RAM_TOTAL=$(free -h | grep Mem | awk '{print $2}')
-RAM_USED=$(free -h | grep Mem | awk '{print $3}')
-RAM_PERC=$(free -k | grep Mem | awk '{printf("%.2f%%"), $3 / $2 * 100}')
-DISK_TOTAL=$(df -h --total | grep total | awk '{print $2}')
-DISK_USED=$(df -h --total | grep total | awk '{print $3}')
-DISK_PERC=$(df -k --total | grep total | awk '{print $5}')
-CPU_LOAD=$(top -bn1 | grep '^%Cpu' | xargs | awk '{printf("%.1f%%"), $2 + $4}')
-LAST_BOOT=$(who -b | awk '{print($3 " " $4)}')
-LVM=$(if [ $(lsblk | grep lvm | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
-TCP=$(grep TCP /proc/net/sockstat | awk '{print $3}')
-USER_LOG=$(who | wc -l)
-IP_ADDR=$(hostname -I | awk '{print $1}')
-MAC_ADDR=$(ip link show | grep link/ether | awk '{print $2}')
-SUDO_LOG=$(grep COMMAND /var/log/sudo/sudo.log | wc -l)
-
-wall "
-       ------------------------------------------------
-       Architecture    : $ARCH
-       Physical CPUs   : $PCPU
-       Virtual CPUs    : $VCPU
-       Memory Usage    : $RAM_USED/$RAM_TOTAL ($RAM_PERC)
-       Disk Usage      : $DISK_USED/$DISK_TOTAL ($DISK_PERC)
-       CPU Load        : $CPU_LOAD
-       Last Boot       : $LAST_BOOT
-       LVM use         : $LVM
-       TCP Connections : $TCP established
-       Users logged    : $USER_LOG
-       Network         : $IP_ADDR ($MAC_ADDR)
-       Sudo            : $SUDO_LOG commands used
-       ------------------------------------------------"
+echo -n "#Architecture : "; uname -a
+#echo:write -n:no new line | uname:system informations -a:all
+echo -n "#CPU physical : "; nproc --all
+#nproc:nombre de processeurs physiques (lscpu)
+echo -n "#vCpu : "; cat /proc/cpuinfo | grep processor | wc -l
+#/proc/cpuinfo:info cpu |grep : search | wc : count -l : lines
+echo -n "#Memory Usage: "; free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2}'
+#free:display amount of free and used memory in the systen -m:mebibytes | awk: scripting language for text processing NR : line 2
+echo -n "#Disk Usage: "; df --total -h | awk '$1 == "total" {printf("%d/%dGb (%.2f%%)\n", $3, $2, $3 / $2)}'
+#df:report file system disk space usage -h:human readble: size in power of 1024 | $1 == total : line total
+echo -n "#CPU Load: "; top -bn1 | grep '%Cpu(s)' | awk '{printf("%.2f%%\n", $2 + $4)}'
+#top :afficher les taches execut2e par cpu real time -b : traitement par lot battch mode -n : nombre d'iteration (actualiser) | us : user cpu time, ni :user nice cpu time low priority
+echo -n "#Last boot: "; who -b | awk '{print $3 " " $4}'
+#who : show who is logged on -b : boot
+echo -n "#LVM use: "; lsblk | grep lvm | awk '{if ($1) {print "yes"; exit;} else {print "no"} }'
+echo -n "#Connection TCP: ";cat /proc/net/sockstat | awk '$1 == "TCP:" {print $3}' | tr '\n' ' ' &&  echo "ESTABLISHED"
+#/proc/net/sockstat : connection info | TCP : Transmission Control Protocol | muse : the number of TCP sockets in use (listening)
+echo -n "#User log: "; users | wc -w
+#users: user logged in | wc - w :count words
+echo -n "#Network: "; echo -n "IP " && hostname -I | tr '\n' '(' && ip link | awk '$1 == "link/ether" {print $2}' | tr '\n' ')' && printf "\n"
+#hostname -I : IP | ip link : network device configuration | tr : translate or delete
+echo -n "#SUDO: "; grep "COMMAND" /var/log/sudo/sudo.log | wc -l | tr '\n' ' ' && echo "cmd"
